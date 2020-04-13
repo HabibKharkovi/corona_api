c9e74524775bef8afb8b6ec1776ebd4160d34aff
sabbar <Sabbar.iplex@gmail.com>
2020-04-13 15:53:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+const request = require('request');
+const cheerio = require("cheerio");
+
+app.get('/coronaviruses', cors(), (req, res) =>
+
+    request("https://www.worldometers.info/coronavirus/", function (error, response, body) {
+        if (error) {
+            res.send(response.statusCode);
+        }
+        var country = [];
+        var $ = cheerio.load(body);
+        $('#main_table_countries_today>tbody>tr').not('tr[style="display: none"]').each(function (index, element) {
+            country[index] = {};
+            country[index]['country'] = $(element).find('td:nth-child(1)').text().trim();
+            country[index]['totalCases'] = $(element).find('td:nth-child(2)').text().trim().replace(/[+,]/g, '') * 1;
+            country[index]['newCases'] = $(element).find('td:nth-child(3)').text().trim().replace(/[+,]/g, '') * 1;
+            country[index]['totalDeaths'] = $(element).find('td:nth-child(4)').text().trim().replace(/[+,]/g, '') * 1;
+            country[index]['newDeath'] = $(element).find('td:nth-child(5)').text().trim().replace(/[+,]/g, '') * 1;
+            country[index]['totalRecovered'] = $(element).find('td:nth-child(6)').text().trim().replace(/[+,]/g, '') * 1;
+            country[index]['activeCases'] = $(element).find('td:nth-child(7)').text().trim().replace(/[+,]/g, '') * 1;
+            country[index]['seriousCases'] = $(element).find('td:nth-child(8)').text().trim().replace(/[+,]/g, '') * 1;
+            country[index]['totalCasesperOneMillionPopulation'] = $(element).find('td:nth-child(9)').text().trim().replace(/[+,]/g, '') * 1;
+        });
+
+        res.json(country);
+    })
+)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
