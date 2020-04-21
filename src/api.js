7ff0c0dbd17def4ce6dc5cff55af796c80c8fca0
habib <--global>
2020-04-21 16:15:28 -0700

    resolved cors issue

--- src/api.js
+++ src/api.js
@@ -19,21 +19,24 @@ router.get('/', cors(), (req, res) =>
         $('#main_table_countries_today>tbody>tr').not('tr[style="display: none"]').each(function (index, element) {
             country[index] = {};
             country[index]['country'] = $(element).find('td:nth-child(1)').text().trim();
-            country[index]['totalCases'] = $(element).find('td:nth-child(2)').text().trim().replace(/[+,]/g, '') * 1;
-            country[index]['newCases'] = $(element).find('td:nth-child(3)').text().trim().replace(/[+,]/g, '') * 1;
-            country[index]['totalDeaths'] = $(element).find('td:nth-child(4)').text().trim().replace(/[+,]/g, '') * 1;
-            country[index]['newDeath'] = $(element).find('td:nth-child(5)').text().trim().replace(/[+,]/g, '') * 1;
-            country[index]['totalRecovered'] = $(element).find('td:nth-child(6)').text().trim().replace(/[+,]/g, '') * 1;
-            country[index]['activeCases'] = $(element).find('td:nth-child(7)').text().trim().replace(/[+,]/g, '') * 1;
-            country[index]['seriousCases'] = $(element).find('td:nth-child(8)').text().trim().replace(/[+,]/g, '') * 1;
-            country[index]['totalCasesperOneMillionPopulation'] = $(element).find('td:nth-child(9)').text().trim().replace(/[+,]/g, '') * 1;
+            country[index]['totalCases'] = $(element).find('td:nth-child(2)').text().trim();
+            country[index]['newCases'] = $(element).find('td:nth-child(3)').text().trim();
+            country[index]['totalDeaths'] = $(element).find('td:nth-child(4)').text().trim();
+            country[index]['newDeath'] = $(element).find('td:nth-child(5)').text().trim();
+            country[index]['totalRecovered'] = $(element).find('td:nth-child(6)').text().trim();
+            country[index]['activeCases'] = $(element).find('td:nth-child(7)').text().trim();
+            country[index]['seriousCases'] = $(element).find('td:nth-child(8)').text().trim();
+            country[index]['totalCasesperOneMillionPopulation'] = $(element).find('td:nth-child(9)').text().trim();
         });
 
         res.json(country);
     })
 )
+app.use('/.netlify/functions/api', cors(), router)
 
-app.use('/.netlify/functions/api', router)
 module.exports.handler = serverless(app);
+
+
+// .replace(/[+,]/g, '') * 1
 
 
